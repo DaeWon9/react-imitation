b269b9efe272806af55f84ebd68cb7da54425484
DaeWon9 <qkreodnjs97@naver.com>
2024-11-24 16:42:55 +0900

    feat: TagType 추가

--- src/lib/node/types.ts
+++ src/lib/node/types.ts
@@ -1,4 +1,5 @@
-export interface Props {
+// Props 타입 정의
+export type Props = {
   [key: string]:
     | string
     | number
@@ -6,15 +7,50 @@ export interface Props {
     | object
     | ((event: Event) => void)
     | undefined;
-  // on으로 시작하는 이벤트들을 EventListener로 지정
-  [key: `on${string}`]: EventListener | undefined;
-}
+  className?: string;
+};
 
+// 자식 요소 타입 정의
 export type Children = string | HTMLElement | Children[];
 
+// 시멘틱 태그 타입 정의
+export type SemanticTags =
+  | 'header'
+  | 'footer'
+  | 'section'
+  | 'article'
+  | 'nav'
+  | 'aside'
+  | 'main'
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'ul'
+  | 'ol'
+  | 'li'
+  | 'p'
+  | 'span'
+  | 'div';
+
+// 폼 요소 타입 정의
+export type FormTags =
+  | 'button'
+  | 'input'
+  | 'textarea'
+  | 'select'
+  | 'option'
+  | 'label'
+  | 'form';
+
+export type TagType = SemanticTags | FormTags;
+
+// createElement 함수의 props 타입 정의
 export interface CreateElementProps {
-  tag: string;
+  tag: TagType;
   className?: string;
   props?: Props;
-  children?: Children[];
+  children: Children[];
 }
