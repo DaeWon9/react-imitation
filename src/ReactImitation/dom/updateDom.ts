892d029be5b11eaaf4de9aed426e1b364f27e4f9
DaeWon9 <qkreodnjs97@naver.com>
2024-12-04 16:26:26 +0900

    refactor: rename initVDOM -> prevVDOM && newVDOM -> nextVDOM

--- src/ReactImitation/dom/updateDom.ts
+++ src/ReactImitation/dom/updateDom.ts
@@ -11,16 +11,16 @@ import { setAttributes } from './setAttributes';
  * - 이 과정에서 불필요한 렌더링을 최소화하기 위해 효율적인 비교 및 업데이트가 이루어집니다.
  *
  * @param $parent - DOM 업데이트를 적용할 부모 요소입니다. 기본값은 루트 요소입니다.
- * @param newVDOM - 새로운 가상 DOM입니다.
- * @param initVDOM - 이전의 가상 DOM입니다.
+ * @param nextVDOM - 새로운 가상 DOM입니다.
+ * @param prevVDOM - 이전의 가상 DOM입니다.
  */
 export function updateDOM(
   $parent: HTMLElement = getRoot(),
-  newVDOM: VDOM = getNewVDOM(),
-  initVDOM: VDOM = getVDOM()
+  nextVDOM: VDOM = getNewVDOM(),
+  prevVDOM: VDOM = getVDOM()
 ): void {
-  updateElement($parent, newVDOM, initVDOM);
-  setVDOM(newVDOM);
+  updateElement($parent, nextVDOM, prevVDOM);
+  setVDOM(nextVDOM);
 }
 
 /**
@@ -28,23 +28,25 @@ export function updateDOM(
  *
  * - 텍스트 노드가 변경되었는지, 또는 요소 노드가 변경되었는지를 판단하여 반환합니다.
  *
- * @param initVDOM - 이전의 가상 DOM 객체입니다.
- * @param newVDOM - 새로운 가상 DOM 객체입니다.
+ * @param prevVDOM - 이전의 가상 DOM 객체입니다.
+ * @param nextVDOM - 새로운 가상 DOM 객체입니다.
  * @returns 두 가상 DOM 객체가 다르면 true, 같으면 false를 반환합니다.
  */
 function isChanged(
-  initVDOM: VDOM | TextVDOM | undefined,
-  newVDOM: VDOM | TextVDOM | undefined
+  prevVDOM: VDOM | TextVDOM | undefined,
+  nextVDOM: VDOM | TextVDOM | undefined
 ): boolean {
-  const isTextInitVDOM = isTextVDOM(initVDOM);
-  const isTextNewVDOM = isTextVDOM(newVDOM);
+  const isTextprevVDOM = isTextVDOM(prevVDOM);
+  const isTextnextVDOM = isTextVDOM(nextVDOM);
 
   return (
-    isTextInitVDOM !== isTextNewVDOM ||
-    (isTextInitVDOM &&
-      isTextNewVDOM &&
-      !Object.is(initVDOM.value, newVDOM.value)) ||
-    (!isTextInitVDOM && !isTextNewVDOM && !Object.is(initVDOM?.el, newVDOM?.el))
+    isTextprevVDOM !== isTextnextVDOM ||
+    (isTextprevVDOM &&
+      isTextnextVDOM &&
+      !Object.is(prevVDOM.value, nextVDOM.value)) ||
+    (!isTextprevVDOM &&
+      !isTextnextVDOM &&
+      !Object.is(prevVDOM?.el, nextVDOM?.el))
   );
 }
 
@@ -55,56 +57,56 @@ function isChanged(
  * - 자식 요소들이 있을 경우 재귀적으로 자식 요소들에 대해서도 동일한 작업을 수행합니다.
  *
  * @param $parent - 부모 DOM 요소입니다.
- * @param newVDOM - 새로운 가상 DOM 객체입니다.
- * @param initVDOM - 이전의 가상 DOM 객체입니다.
+ * @param nextVDOM - 새로운 가상 DOM 객체입니다.
+ * @param prevVDOM - 이전의 가상 DOM 객체입니다.
  */
 function updateElement(
   $parent: HTMLElement | Text,
-  newVDOM: TextVDOM | VDOM | undefined,
-  initVDOM?: TextVDOM | VDOM | undefined
+  nextVDOM: TextVDOM | VDOM | undefined,
+  prevVDOM?: TextVDOM | VDOM | undefined
 ): void {
-  const $current = initVDOM?.current;
+  const $current = prevVDOM?.current;
 
   // 초기 가상 DOM이 없거나 텍스트 노드 값이 없으면 새로운 DOM을 생성하여 추가
-  if (!initVDOM || (isTextVDOM(initVDOM) && !initVDOM.value)) {
-    const $next = createDOM(newVDOM);
+  if (!prevVDOM || (isTextVDOM(prevVDOM) && !prevVDOM.value)) {
+    const $next = createDOM(nextVDOM);
     if ($next) {
       $parent.appendChild($next);
     }
   }
   // 새로운 가상 DOM이 없거나 텍스트 노드 값이 없으면 기존 DOM을 제거
-  else if (!newVDOM || (isTextVDOM(newVDOM) && !newVDOM.value)) {
+  else if (!nextVDOM || (isTextVDOM(nextVDOM) && !nextVDOM.value)) {
     if ($current) {
       $parent.removeChild($current);
     }
   }
   // 가상 DOM이 변경되었다면 기존 DOM을 교체
-  else if (isChanged(initVDOM, newVDOM)) {
-    const $next = createDOM(newVDOM);
+  else if (isChanged(prevVDOM, nextVDOM)) {
+    const $next = createDOM(nextVDOM);
     if ($current && $next) {
       $current.replaceWith($next);
-      newVDOM.current = $next;
+      nextVDOM.current = $next;
     }
   }
   // 가상 DOM이 모두 엘리먼트인 경우 자식 요소들에 대해 재귀적으로 처리
-  else if (!isTextVDOM(initVDOM) && !isTextVDOM(newVDOM)) {
+  else if (!isTextVDOM(prevVDOM) && !isTextVDOM(nextVDOM)) {
     const length = Math.max(
-      initVDOM.children?.length || 0,
-      newVDOM.children?.length || 0
+      prevVDOM.children?.length || 0,
+      nextVDOM.children?.length || 0
     );
-    newVDOM.current = $current;
+    nextVDOM.current = $current;
 
     for (let i = 0; i < length; i++) {
       if ($current) {
-        updateElement($current, newVDOM.children?.[i], initVDOM.children?.[i]);
+        updateElement($current, nextVDOM.children?.[i], prevVDOM.children?.[i]);
       }
     }
   } else {
-    newVDOM.current = $current;
+    nextVDOM.current = $current;
   }
 
   // 새로운 가상 DOM이 엘리먼트인 경우 속성을 설정
-  if (!isTextVDOM(newVDOM) && newVDOM) {
-    setAttributes(newVDOM.props, $current);
+  if (!isTextVDOM(nextVDOM) && nextVDOM) {
+    setAttributes(nextVDOM.props, $current);
   }
 }
