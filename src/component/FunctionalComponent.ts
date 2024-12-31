23cac39fac1bd17643c639cc33251f9ea5afc749
DaeWon9 <qkreodnjs97@naver.com>
2024-12-31 21:50:59 +0900

    feat: useEffect test

--- src/component/FunctionalComponent.ts
+++ src/component/FunctionalComponent.ts
@@ -1,9 +1,22 @@
 import { useState, createElement } from '../ReactImitation';
+import { useEffect } from '../ReactImitation/hooks/useEffect';
 import { getCurrentKey } from '../ReactImitation/vdom/store';
 import styles from './FunctionalComponent.module.css';
 
 export function FunctionalComponent(props: any) {
   const [testState, setTestState] = useState(1);
+
+  useEffect(() => {
+    console.log(getCurrentKey(), '가 마운트 되었음');
+  }, []);
+
+  useEffect(() => {
+    console.log('외부의 state가 변경되었음', props.count);
+  }, [props.count]);
+
+  useEffect(() => {
+    console.log('내부의 state가 변경되었음', testState);
+  }, [testState]);
 
   return createElement({
     el: 'div',
